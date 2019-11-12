9c9d23e186c272859c88ff9ba0c18a354b93e3a8
Zak the Logician <zaklogician@github.com>
2019-11-12 11:57:15 +0000

    Implement Isabelle proof using Miki's suggestion

--- Induct.thy
+++ Induct.thy
@@ -6,8 +6,8 @@ begin
 inductive "downset" :: "(nat \<Rightarrow> bool) \<Rightarrow> nat \<Rightarrow> bool" where
   "downset_lit": "\<phi> x \<Longrightarrow> downset \<phi> x" |
   "downset_pred": "downset \<phi> (Suc x) \<Longrightarrow> downset \<phi> x"
-print_theorems
 
 lemma
   "downset (downset \<phi>) x \<Longrightarrow> downset \<phi> x"
-  oops
+  apply (induct "downset \<phi>" x rule: downset.induct)
+  by (auto simp: downset_pred)
